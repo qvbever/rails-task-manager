195a82d5920ba298aaa910f9ac99a59fe86491f1
quentin vanbever <quentin.vanbever@gmail.com>
2024-09-20 21:02:50 +0200

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  # Read all
+  # get "tasks", to: "tasks#index"
+  # # Create
+  # get "tasks/new", to: "tasks#new", as: :new_task
+  # post "tasks", to: "tasks#create"
+  # # Read one
+  # get "tasks/:id", to: "tasks#show", as: :task
+  # # Update
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # patch "tasks/:id", to: "tasks#update"
+
+  # delete "tasks/:id", to: "tasks#destroy"
+  resources :tasks
+
 end
